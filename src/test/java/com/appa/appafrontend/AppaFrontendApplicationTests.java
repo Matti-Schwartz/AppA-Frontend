0fd3b1ceaf05acaa77ebea3f5b25d917dfc1c46f
Matti Schwartz <s87219@bht-berlin.de>
2023-02-19 13:35:16 +0100

    Initial commit

---
+++ src/test/java/com/appa/appafrontend/AppaFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appa.appafrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppaFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
