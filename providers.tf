eba4ba84d5e568108ab7af45eafe482be7c976b3
Matti Schwartz <s87219@bht-berlin.de>
2023-03-01 17:25:19 +0100

    Fix providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  shared_credentials_file = "$HOME/.aws/credentials"
+  shared_credentials_files = "$HOME/.aws/credentials"
   region  = "us-east-1"
   profile = "default"
 }
