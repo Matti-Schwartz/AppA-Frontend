3c15ae333c300a19fd38cb7a65b09b3dda15d79c
Matti Schwartz <s87219@bht-berlin.de>
2023-03-03 20:33:36 +0100

    Change text in MainController

--- src/main/java/com/appa/appafrontend/MainController.java
+++ src/main/java/com/appa/appafrontend/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "Per aspera ad astra.";
     }
 }
